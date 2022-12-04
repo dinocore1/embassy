c92984cd52cc657bd32acb6ba7d90e99b9a6d76f
Paul Soucy <paulsoucy1@gmail.com>
2022-12-04 10:51:27 -0500

    wip

--- embassy-gd32/src/lib.rs
+++ embassy-gd32/src/lib.rs
@@ -1,7 +1,15 @@
 #![no_std]
 
+#[cfg(not(any(
+    feature = "gd32e503",
+)))]
+compile_error!("No chip feature activated. You must activate one of the chip features.");
+
+
 cfg_if::cfg_if! {
     if #[cfg(feature = "gd32e503")] {
         //pub use gd32e5::
     }
 }
+
+pub use embassy_cortex_m::executor;
